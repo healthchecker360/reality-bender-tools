c09b4cfad544cc4fee6693f5fe8dd28aa1d58030
healthchecker360 <healthchecker360@gmail.com>
2025-12-25 18:02:23 +0500

    Update BeforeAfterSlider.js

--- frontend/src/components/BeforeAfterSlider.js
+++ frontend/src/components/BeforeAfterSlider.js
@@ -4,18 +4,34 @@ function BeforeAfterSlider({ original, processed }) {
   const [slider, setSlider] = useState(50);
 
   return (
-    <div className="relative w-full h-96 overflow-hidden mb-4">
-      <img src={original} alt="Original" className="absolute top-0 left-0 w-full h-full object-contain" />
-      <div className="absolute top-0 left-0 h-full overflow-hidden" style={{ width: `${slider}%` }}>
-        <img src={processed} alt="Processed" className="w-full h-full object-contain" />
+    <div className="relative w-full h-96 mb-4 border rounded overflow-hidden">
+      {/* Original Image */}
+      <img
+        src={original}
+        alt="Original"
+        className="absolute top-0 left-0 w-full h-full object-contain"
+      />
+
+      {/* Processed Image with slider width */}
+      <div
+        className="absolute top-0 left-0 h-full overflow-hidden"
+        style={{ width: `${slider}%` }}
+      >
+        <img
+          src={processed}
+          alt="Processed"
+          className="w-full h-full object-contain"
+        />
       </div>
+
+      {/* Slider Control */}
       <input
         type="range"
         min="0"
         max="100"
         value={slider}
         onChange={(e) => setSlider(e.target.value)}
-        className="absolute bottom-0 left-0 w-full"
+        className="absolute bottom-0 left-0 w-full accent-blue-500"
       />
     </div>
   );
